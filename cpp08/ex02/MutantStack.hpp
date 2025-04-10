ed737fef89c18c39d09384deababeef58640eb92
christoph Koennecke <ckonneck@student.42wolfsburg.de>
2025-04-10 14:59:02 +0200

    m

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,5 +6,35 @@ template <typename T,typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
 	public:
-		
+		MutantStack();
+		MutantStack(const MutantStack& other);
+		MutantStack& operator=(const MutantStack& other);
+		~MutantStack();
+	
+	typedef typename Container::iterator iterator;
+	iterator begin();
+    iterator end();
 };
+
+template <typename T,typename Container> 
+MutantStack<T, Container>::MutantStack() : std::stack<T, Container>() {}
+
+template <typename T,typename Container>
+MutantStack<T, Container>::MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {}
+
+template <typename T,typename Container>
+MutantStack<T, Container>& MutantStack<T, Container>::operator=(const MutantStack& other)
+{
+	if (this != &other)
+	std::stack<T, Container>::operator=(other);
+	return *this;
+}
+
+template <typename T,typename Container> 
+typename MutantStack<T, Container>::iterator MutantStack<T, Container>::end() {return this->c.end();}
+
+template <typename T,typename Container> 
+typename MutantStack<T, Container>::iterator MutantStack<T, Container>::begin() {return this->c.begin();}
+
+template <typename T,typename Container> 
+MutantStack<T, Container>::~MutantStack() {}
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -2,7 +2,53 @@
 
 int main()
 {
-	MutantStack<int> stack;
-	stack.push(1);
-	std::cout << stack.size() << std::endl;
+MutantStack<int> mstack;
+mstack.push(5);
+mstack.push(17);
+std::cout << mstack.top() << std::endl;
+mstack.pop();
+std::cout << mstack.size() << std::endl;
+mstack.push(3);
+mstack.push(5);
+mstack.push(737);
+//[...]
+mstack.push(0);
+MutantStack<int>::iterator it = mstack.begin();
+MutantStack<int>::iterator ite = mstack.end();
+++it;
+--it;
+while (it != ite)
+{
+std::cout << *it << std::endl;
+++it;
 }
+std::stack<int> s(mstack);
+return 0;
+}
+
+// #include <list>
+// int main()
+// {
+//     std::list<int> mlist;
+//     mlist.push_back(5);      // push -> push_back
+//     mlist.push_back(17);
+//     std::cout << mlist.back() << std::endl;   // top -> back
+//     mlist.pop_back();        // pop -> pop_back
+//     std::cout << mlist.size() << std::endl;
+//     mlist.push_back(3);
+//     mlist.push_back(5);
+//     mlist.push_back(737);
+//     //[...]
+//     mlist.push_back(0);
+//     std::list<int>::iterator it = mlist.begin();
+//     std::list<int>::iterator ite = mlist.end();
+//     ++it;
+//     --it;
+//     while (it != ite)
+//     {
+//         std::cout << *it << std::endl;
+//         ++it;
+//     }
+//     std::list<int> s(mlist);  // Copy constructor works the same
+//     return 0;
+// }
